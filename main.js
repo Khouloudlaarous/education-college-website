6cf936076a8aa86eeb4a3c13ac2191e9f06077ca
Khouloudlaarous <khouloudaissa@gmail.com>
2023-05-30 20:17:25 +0100

    files project

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window,scrollY > 0)
+})
+
+
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    })
+})
