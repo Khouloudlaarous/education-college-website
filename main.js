617b3e0b1552be2d36ac39c4ddefd9d74a9c24ec
Khouloudlaarous <khouloudaissa@gmail.com>
2023-06-19 00:13:28 +0100

    make design reponsive & create a drop down menu

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window,scrollY > 0)
 })
 
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
 
 
-const faqs = document.querySelectorAll('.faq');
-
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-    })
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
 })
