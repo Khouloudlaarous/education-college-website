8320c30a0b3ad24b6909b2f1429c38a351b0fbf8
Khouloudlaarous <khouloudaissa@gmail.com>
2023-06-19 00:32:08 +0100

    add click btn close event

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ menuBtn.addEventListener('click', () => {
     closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
 })
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+menuBtn.addEventListener('click', closeNav)
+
