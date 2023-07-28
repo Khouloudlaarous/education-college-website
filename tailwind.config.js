0a48dd22ff30b2a3a18e5c8a50a1c10f36c396c4
Khouloudlaarous <khouloudaissa@gmail.com>
2023-07-28 21:19:45 +0100

    add tailwind css to the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['*.html'],
+  corePlugins: {
+    preflight: false,
+
+  },
+  prefix: 'tw-',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
